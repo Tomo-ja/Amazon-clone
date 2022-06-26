7590494a0f830433fc4565fdbd42a7069167e53f
Tomo-ja <tomonao.hj@gmail.com>
2022-06-26 16:09:19 -0700

    init

---
+++ src/Firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyCtLOZ6XYRLyiQaB334-kquj84X3FYsUsM",
+	authDomain: "fir-3d151.firebaseapp.com",
+	projectId: "fir-3d151",
+	storageBucket: "fir-3d151.appspot.com",
+	messagingSenderId: "987482985000",
+	appId: "1:987482985000:web:0a50fc4e06371fbc50db1d",
+	measurementId: "G-3B2ZZX9M9K"
+};
