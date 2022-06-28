ca035ca6c364228d38334b51b966c7fd8d992bb4
Tomo-ja <tomonao.hj@gmail.com>
2022-06-28 11:53:28 -0700

    set up fire base config

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,4 +1,8 @@
-export const firebaseConfig = {
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
 	apiKey: "AIzaSyCtLOZ6XYRLyiQaB334-kquj84X3FYsUsM",
 	authDomain: "fir-3d151.firebaseapp.com",
 	projectId: "fir-3d151",
@@ -7,3 +11,10 @@ export const firebaseConfig = {
 	appId: "1:987482985000:web:0a50fc4e06371fbc50db1d",
 	measurementId: "G-3B2ZZX9M9K"
 };
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const db = getFirestore(firebaseApp)
+const auth = getAuth(firebaseApp)
+
+export { db, auth }
