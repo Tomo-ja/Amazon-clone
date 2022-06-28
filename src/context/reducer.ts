815973fdd12bf07227297839ee18f766b4cc2cc7
Tomo-ja <tomonao.hj@gmail.com>
2022-06-28 10:41:53 -0700

    create remove item function

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -20,8 +20,18 @@ export const reducer = (state: context, action: reducerAction) => {
 				basket: [...state.basket, action.item]
 			}
 		case 'REMOVE_FROM_BASKET':
+			const index = state.basket.findIndex(
+				basketItem => basketItem.id === action.item.id
+			)
+			let newBasket = [...state.basket]
+			if (index >= 0){
+				newBasket.splice(index, 1)
+			} else {
+				console.warn("something is wrong here")
+			}
 			return {
-				...state
+				...state,
+				basket: newBasket
 			}
 		default:
 			return state
