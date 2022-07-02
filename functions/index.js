d8456272ebc7ac801fee18d14323b320edf54e4b
Tomo-ja <tomonao.hj@gmail.com>
2022-07-02 12:51:34 -0700

    add basic backend code

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_51LGt3tD8Xqz5qk8QomeFC4kSg4pgbuJttFbm71D56s4wrCy5hN5N9qnY0TfWokmlJMcLbTiBzuyF3cBthNrb62t500A2btM3Jg");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors({origin: true}));
+app.use(express.json());
+
+app.get("/", (requrest, response) => response.status(200).send("hello world"));
+
+exports.api = functions.https.onRequest(app)
