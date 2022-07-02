a4e4fce237d35675880da18545900d27e0ac6a8e
Tomo-ja <tomonao.hj@gmail.com>
2022-07-01 17:56:46 -0700

    create api request for payment

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,14 +1,51 @@
-import React, { Dispatch } from 'react'
+import React, { Dispatch, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import CurrencyFormat from 'react-currency-format'
+
+import CheckoutProduct from './CheckoutProduct'
+
 import { useStateValue } from '../context/StateProvider'
+import { getBasketTotal } from '../context/reducer'
 import context from '../interface/Context'
 import reducerAction from '../interface/ReducerAction'
-import CheckoutProduct from './CheckoutProduct'
+import axios from 'axios'
 
 export default function Payment() {
 
 	const [{ basket, user }, dispatch ]: [context, Dispatch<reducerAction>] = useStateValue()
+	const [error, setError] = useState<string | null>(null)
+	const [disabled, setDisabled] = useState(true)
+	const [processing, setProcessing] = useState(false)
+	const [succeeded, setSucceeded] = useState(false)
+	const [clientSecret, setClientSecret] = useState(true)
 
+	const stripe = useStripe()
+	const elements = useElements()
+
+	async function handleSubmit(e: React.SyntheticEvent){
+		e.preventDefault()
+		setProcessing(true)
+
+		// const payload = await stripe
+	}
+
+	function handleChange(e: StripeCardElementChangeEvent){
+		setDisabled(e.empty)
+		setError(e.error ? e.error.message: "" )
+	}
+
+	useEffect(()=>{
+		const getClientSecret = async () => {
+			const response = await axios({
+				method: 'post',
+				url: `/payments/create?total=${getBasketTotal(basket) * 100}`
+			})
+			setClientSecret(response.data.clientSecret)
+		}
+		getClientSecret()
+	}, [basket])
   return (
 	<div className='payment'>
 		<div className="payment__container">
@@ -44,7 +81,28 @@ export default function Payment() {
 					<h3>Payment Method</h3>
 				</div>
 				<div className="payment__section__details">
-
+					<form onSubmit={handleSubmit}>
+						<CardElement onChange={handleChange}/>
+						<div className="payment__section__details__price">
+							<CurrencyFormat
+								renderText={(value) => (
+									<h3>Order Total: {value}</h3>
+								)}
+								decimalScale={2}
+								value={getBasketTotal(basket)}
+								displayType={"text"}
+								thousandSeparator={true}
+								prefix={"$"}
+							/>
+							<button 
+								className='payment__section__details__button'
+								disabled={processing || disabled || succeeded}
+							>
+								{processing ? "Processing": "Buy Now"}
+							</button>
+						</div>
+						{ error && <div>{error}</div>}
+					</form>
 				</div>
 			</section>
 		</div>
